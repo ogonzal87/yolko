6301cd12f0a3598c1a2c1ec792d1e649c12a0f5d
Oscar Gonzalez <ogonzal87@gmail.com>
2015-03-29 14:44:19 -0400

    commit my stash

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -1,8 +1,10 @@
-myApp.controller('MeetingsController', function($scope, $firebaseArray) {
+myApp.controller('MeetingsController', function($scope, $rootScope, $firebaseArray) {
 
-	var ref = new Firebase('https://pollit.firebaseio.com/meetings');
+	var ref = new Firebase('https://pollit.firebaseio.com/meetings/users/' + $rootScope.currentUser.$getKey + '/meetings');
 	var meetings = $firebaseArray(ref);
 	$scope.meetings = meetings;
+
+	console.log($rootScope.currentUser.$getKey);
 
 	$scope.addMeeting = function() {
 		meetings.$add({
