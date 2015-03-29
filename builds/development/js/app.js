0e5c246e7b9b66da6be11760d5a37598af74d5cd
Oscar Gonzalez <ogonzal87@gmail.com>
2015-03-29 14:32:25 -0400

    workined on saving items to individual users but could not figure it out.

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -2,15 +2,6 @@ var myApp = angular.module('myApp', ['ngRoute', 'appControllers', 'firebase']);
 
 var appControllers = angular.module('appControllers', ['firebase']);
 
-myApp.run(['$rootScope', '$location', function($rootScope, $location) {
-	$rootScope.$on('$routeChangeError', function(event, next, previous, error) {
-		if(error === 'AUTH_REQUIRED') {
-			$rootScope.message = 'Sorry, you must log in to access that page!';
-			$location.path('/login')
-		}
-	});
-}]);
-
 myApp.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
 		.when('/login', {
@@ -24,10 +15,6 @@ myApp.config(['$routeProvider', function($routeProvider) {
 		.when('/meetings', {
 			templateUrl: 'views/meetings.html',
 			controller: 'MeetingsController', 
-			resolve: {
-				currentAuth: function(Authentication) {
-					return Authentication.requireAuth();
-				}
 			}
 		})
 		.otherwise({
