bbaf0540e9a4afd6b02f2ce5d027dc0bc769e6a8
Oscar Gonzalez <ogonzal87@gmail.com>
2015-03-29 01:38:32 -0400

    completed auth

---
+++ builds/development/js/controllers/status.js
@@ -0,0 +1,7 @@
+myApp.controller('StatusController', function($scope, $location, Authentication) {
+
+	$scope.logout = function() {
+		Authentication.logout();
+		$location.path('/login');
+	};
+});
--- builds/development/js/services/authentication.js
+++ builds/development/js/services/authentication.js
@@ -1,7 +1,18 @@
-myApp.factory('Authentication', function($firebase, $firebaseAuth, $routeParams, $location) {
+myApp.factory('Authentication', function($firebaseArray, $firebaseObject, $rootScope, $firebaseAuth, $routeParams, $location) {
 
 	var ref  = new Firebase('https://pollit.firebaseio.com/');
 	var auth = $firebaseAuth(ref);
+
+	auth.$onAuth(function(authUser) {
+		if (authUser) {
+			var ref = new Firebase('https://pollit.firebaseio.com/users/' + authUser.uid);
+			var user = $firebaseArray(ref);
+			$rootScope.currentUser = user;
+			console.log(user);
+		} else {
+			$rootScope.currentUser = '';
+		}
+	});
 
 	//Temporary object
 	var myObject = {
@@ -13,12 +24,36 @@ myApp.factory('Authentication', function($firebase, $firebaseAuth, $routeParams,
 			}); //authWithPassword
 		}, //login
 
+		logout: function(user) {
+			return auth.$unauth();
+		}, //login
+
 		register: function(user) {
 			return auth.$createUser({
 				email: user.email, 
 				password: user.password
-			});
-		}//register
+			}).then(function(regUser) {
+				var ref = new Firebase('https://pollit.firebaseio.com/users');
+				var firebaseUsers = $firebaseArray(ref);
+
+				firebaseUsers.$add({
+					date	  	: Firebase.ServerValue.TIMESTAMP,
+					regUser 	: regUser.uid,
+					firstname	: user.firstName,
+					lastname	: user.lastName,
+					email			: user.email
+				});
+			}); //promise
+		}, //register
+
+		requireAuth: function() {
+			return auth.$requireAuth();
+		}, //requireauth
+
+		requireForAuth: function() {
+			return auth.$waitForAuth();
+		} //wait until user is authenticated
+
 	};
 	return myObject;
 }); //myApp Factory
