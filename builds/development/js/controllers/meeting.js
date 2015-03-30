66603b79a9d1ee6e110f00e78b8863ea6170f18b
Oscar Gonzalez <ogonzal87@gmail.com>
2015-03-30 09:01:09 -0400

    added bootstrap to app

--- builds/development/js/controllers/meeting.js
+++ builds/development/js/controllers/meeting.js
@@ -3,15 +3,14 @@ myApp.controller('MeetingController', function($scope, $rootScope, $firebaseArra
 	//FIREBASE REFERENCE
   var firebaseUrl = 'https://pollit.firebaseio.com/';
 
-  // GET CLICKS AS ARRAY
+  //GET CLICKS AS ARRAY
   $scope.voteUps = $firebaseArray(new Firebase(firebaseUrl + '/voteUps'));
   $scope.voteNeutrals = $firebaseArray(new Firebase(firebaseUrl + '/voteNeutrals'));
   $scope.voteDowns = $firebaseArray(new Firebase(firebaseUrl + '/voteDowns'));
 
-
   $scope.voteUp = function() {
     $scope.voteUps.$add(1);
-    console.log('There are ' + ($scope.voteUps.length+1) + ' voteUps!');
+    console.log('There are ' + ($scope.voteUps.length) + ' voteUps!');
   }
 
   $scope.neutral = function() {
@@ -24,5 +23,10 @@ myApp.controller('MeetingController', function($scope, $rootScope, $firebaseArra
     console.log('There are ' + ($scope.voteDowns.length+1) + ' voteDowns');
     console.log(($scope.voteUps.length) - ($scope.voteDowns.length));
   }
+
+
+  $scope.counter = $firebaseArray(new Firebase(firebaseUrl + '/voteUps')).length;
+
+
 
 }); //MeetingController
