810a7bba7ee0b3c5caa32e1ce1452e9e37572118
Oscar Gonzalez <ogonzal87@gmail.com>
2015-04-05 16:11:32 -0400

    fixed bug with decimals

--- builds/development/js/controllers/meeting.js
+++ builds/development/js/controllers/meeting.js
@@ -38,6 +38,7 @@ myApp.controller('MeetingController', function($timeout, FIREBASE_URL, $scope, $
     $scope.delta = $scope.howManyVoteUps - $scope.howManyVoteDowns;
     $scope.totalVotes = $scope.howManyVoteUps + $scope.howManyVoteDowns;
     $scope.failureRate = ($scope.howManyVoteDowns / $scope.totalVotes) * 100;
+    $scope.zzz = true;
     updateMessage();
     displayYolko(); 
   });//counter
@@ -86,47 +87,47 @@ myApp.controller('MeetingController', function($timeout, FIREBASE_URL, $scope, $
 //Displaying Yolko
   $scope.zzz = true;
   function displayYolko() {
-    $scope.zzz                   = $scope.failureRate < 7 || null;
+    $scope.zzz                   = $scope.failureRate < 7.999999999999 || null;
     $scope.burnt                 = $scope.failureRate >= 96; 
-    $scope.laughing              = $scope.failureRate >= 8 && $scope.failureRate <= 15;
-    $scope.igotthis              = $scope.failureRate >= 16 && $scope.failureRate <= 23;
-    $scope.easy                  = $scope.failureRate >= 24 && $scope.failureRate <= 31;
-    $scope.easywhereisthefood    = $scope.failureRate >= 32 && $scope.failureRate <= 39;
-    $scope.yum                   = $scope.failureRate >= 40 && $scope.failureRate <= 47;
-    $scope.startingtonotgetit    = $scope.failureRate >= 48 && $scope.failureRate <= 55;
-    $scope.idontgetit            = $scope.failureRate >= 56 && $scope.failureRate <= 63;
-    $scope.shocked               = $scope.failureRate >= 64 && $scope.failureRate <= 71;
-    $scope.fire                  = $scope.failureRate >= 72 && $scope.failureRate <= 79;
-    $scope.beaten                = $scope.failureRate >= 80 && $scope.failureRate <= 87;
-    $scope.burnt                 = $scope.failureRate >= 88 && $scope.failureRate <= 95;
+    $scope.laughing              = $scope.failureRate >= 8 && $scope.failureRate <= 15.999999999999;
+    $scope.igotthis              = $scope.failureRate >= 16 && $scope.failureRate <= 23.999999999999;
+    $scope.easy                  = $scope.failureRate >= 24 && $scope.failureRate <= 31.999999999999;
+    $scope.easywhereisthefood    = $scope.failureRate >= 32 && $scope.failureRate <= 39.999999999999;
+    $scope.yum                   = $scope.failureRate >= 40 && $scope.failureRate <= 47.999999999999;
+    $scope.startingtonotgetit    = $scope.failureRate >= 48 && $scope.failureRate <= 55.999999999999;
+    $scope.idontgetit            = $scope.failureRate >= 56 && $scope.failureRate <= 63.999999999999;
+    $scope.shocked               = $scope.failureRate >= 64 && $scope.failureRate <= 71.999999999999;
+    $scope.fire                  = $scope.failureRate >= 72 && $scope.failureRate <= 79.999999999999;
+    $scope.beaten                = $scope.failureRate >= 80 && $scope.failureRate <= 87.999999999999;
+    $scope.burnt                 = $scope.failureRate >= 88 && $scope.failureRate <= 95.999999999999;
   }; //Displaying Yolko
 
     function updateMessage() {
-    if ($scope.failureRate < 7 || null) {
+    if ($scope.failureRate < 7.999999999999 || null) {
         $scope.message = 'ZZZ'; //ZZZ
       } else if ($scope.failureRate >= 96) {
         $scope.message = ''; //burnt
-      } else if ($scope.failureRate >= 8 && $scope.failureRate <= 15) {
+      } else if ($scope.failureRate >= 8 && $scope.failureRate <= 15.999999999999) {
         $scope.message = 'Mmm... This is too easy'; //laughing
-      } else if ($scope.failureRate >= 16 && $scope.failureRate <= 23) {
+      } else if ($scope.failureRate >= 16 && $scope.failureRate <= 23.999999999999) {
         $scope.message = 'Dude, you are killing it out there. Do your thang!'; // igotthis
-      } else if ($scope.failureRate >= 24 && $scope.failureRate <= 31) {
+      } else if ($scope.failureRate >= 24 && $scope.failureRate <= 31.999999999999) {
         $scope.message = 'I feel like partying... Are you with me?'; //easy
-      } else if ($scope.failureRate >= 32 && $scope.failureRate <= 39) {
+      } else if ($scope.failureRate >= 32 && $scope.failureRate <= 39.999999999999) {
         $scope.message = 'HTML? Too easy, where is the food...?'; // easywhereisthefood
-      } else if ($scope.failureRate >= 40 && $scope.failureRate <= 47) {
+      } else if ($scope.failureRate >= 40 && $scope.failureRate <= 47.999999999999) {
         $scope.message = 'Yummmm'; //yum
-      } else if ($scope.failureRate >= 48 && $scope.failureRate <= 55) {
+      } else if ($scope.failureRate >= 48 && $scope.failureRate <= 55.999999999999) {
         $scope.message = 'Psh, is he still speaking English?';//startingtonotgetit
-      } else if ($scope.failureRate >= 56 && $scope.failureRate <= 63) {
+      } else if ($scope.failureRate >= 56 && $scope.failureRate <= 63.999999999999) {
         $scope.message = 'Yep, I am lost';//idontgetit
-      } else if ($scope.failureRate >= 64 && $scope.failureRate <= 71) {
+      } else if ($scope.failureRate >= 64 && $scope.failureRate <= 71.999999999999) {
         $scope.message = 'RSPEC...Wahhhh?';//shocked
-      } else if ($scope.failureRate >= 72 && $scope.failureRate <= 79) {
+      } else if ($scope.failureRate >= 72 && $scope.failureRate <= 79.999999999999) {
         $scope.message = ''; //fire
-      } else if ($scope.failureRate >= 80 && $scope.failureRate <= 87) {
+      } else if ($scope.failureRate >= 80 && $scope.failureRate <= 87.999999999999) {
         $scope.message = 'Imma walk it out'; //beaten
-      } else if ($scope.failureRate >= 88 && $scope.failureRate <= 95) {
+      } else if ($scope.failureRate >= 88 && $scope.failureRate <= 95.999999999999) {
         $scope.message = '';//burnt
       };
   }
